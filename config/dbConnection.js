7a149d270a631fede1ba79916ed577c2a8ef81a6
Sukesh4230 <sukesh1993@gmail.com>
2026-05-30 23:07:46 +0530

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_SITE_URI)
         console.log("Database connected successfully")
     } catch (error) {
         console.log("Error: " + error.message)
